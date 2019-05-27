acc2ac04142fcef77264b49a4e62bbb8625aaa0d
Matt Murphy <mmatt5439@gmail.com>
2019-05-27 18:48:40 +1000

    Accordion deck now doesn't stack when clicking faster than 300ms

--- hydroui.js
+++ hydroui.js
@@ -88,53 +88,67 @@ $(".progress-echo").each(function(index) {
 
 // Accordion
 
+var inProgress = false;
+console.log("Accordion Progress False")
+
 $(document).on('click', '.card-accordion-heading', function(index) {
   var accordionBtn = $(this)
   var accordionContent = $(this).closest('.card-accordion').find('.card-accordion-content')
   var accordionDeck = $(this).closest('.card-accordion-content').find('.card-accordion-content')
 
-  if (!(accordionDeck.hasClass("loading") || accordionDeck.hasClass("unloading") || accordionContent.hasClass("loading") || accordionContent.hasClass("unloading"))) {
+  if (inProgress == false) {
+    inProgress = true;
+    console.log("Progress True")
+    if (!(accordionDeck.hasClass("loading") || accordionDeck.hasClass("unloading") || accordionContent.hasClass("loading") || accordionContent.hasClass("unloading"))) {
 
-    if (accordionContent.hasClass("active")) { // Accordion Closed
-      accordionBtn.find('.card-accordion-icon-flip').css('transform', 'translateY(-50%)')
-      accordionContent.removeClass("active");
+      if (accordionContent.hasClass("active")) { // Accordion Closed
+        accordionBtn.find('.card-accordion-icon-flip').css('transform', 'translateY(-50%)')
+        accordionContent.removeClass("active");
 
-      var heightCalc = $(this).closest('.card-accordion').find('.card-accordion-content').css('height', 'auto').height();
+        var heightCalc = $(this).closest('.card-accordion').find('.card-accordion-content').css('height', 'auto').height();
 
-      accordionContent.height(heightCalc);
-      accordionContent.innerHeight(0);
-      accordionContent.addClass("unloading")
+        accordionContent.height(heightCalc);
+        accordionContent.innerHeight(0);
+        accordionContent.addClass("unloading")
 
-      window.setTimeout(function() {
-        accordionContent.removeClass("unloading");
-        accordionContent.removeAttr("style");
-      }, 300);
+        window.setTimeout(function() {
+          accordionContent.removeClass("unloading");
+          accordionContent.removeAttr("style");
+          inProgress = false;
+          console.log("Progress false closing")
+        }, 300);
 
-    } else { // Accordion Expanded
-      var heightCalc = $(this).closest('.card-accordion').find('.card-accordion-content').css('height', 'auto').innerHeight();
+      } else { // Accordion Expanded
 
-      accordionBtn.find('.card-accordion-icon-flip').css('transform', 'rotate(180deg) translateY(50%)')
+        var heightCalc = $(this).closest('.card-accordion').find('.card-accordion-content').css('height', 'auto').innerHeight();
 
-      accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active').addClass("unloading")
-      accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active.unloading').height(heightCalc);
-      accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active.unloading').removeClass('active')
+        accordionBtn.find('.card-accordion-icon-flip').css('transform', 'rotate(180deg) translateY(50%)')
 
-      window.setTimeout(function() {
-        accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.unloading').removeAttr("style");
-        accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.unloading').removeClass("unloading");
-      }, 0);
+        accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active').addClass("unloading")
+        accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active.unloading').height(heightCalc);
+        accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active.unloading').removeClass('active')
 
-      accordionContent.innerHeight(0);
-      accordionContent.innerHeight(heightCalc + 20);
-      accordionContent.addClass("loading")
+        window.setTimeout(function() {
+          accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.unloading').removeAttr("style");
+          accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.unloading').removeClass("unloading");
+        }, 0);
 
-      window.setTimeout(function() {
-        accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active').removeClass('active');
-        accordionContent.addClass("active");
-        accordionContent.removeClass("loading");
-        accordionContent.removeAttr("style");
-      }, 300);
+        accordionContent.innerHeight(0);
+        accordionContent.innerHeight(heightCalc + 20);
+        accordionContent.addClass("loading")
+
+        window.setTimeout(function() {
+          accordionBtn.closest('.deck.card-accordion-radio').find('.card-accordion-content.active').removeClass('active');
+          accordionContent.addClass("active");
+          accordionContent.removeClass("loading");
+          accordionContent.removeAttr("style");
+          inProgress = false;
+          console.log("Progress false opeing")
+        }, 300);
+
+      }
     }
+
   }
 })
 
