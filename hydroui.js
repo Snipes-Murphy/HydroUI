33d2259cde5c6fbb68542fd9be4ff6bb5c2ace1b
Matt Murphy <mmatt5439@gmail.com>
2020-05-11 14:38:28 +1000

    Added a new table component

--- hydroui.js
+++ hydroui.js
@@ -253,3 +253,80 @@ $(".active-toggler").hover(
   function () { $(this).addClass('active'); },
   function () { $(this).removeClass('active'); }
 );
+
+// Table
+
+$(document).on("click", ".render-table th", function() {
+  indexVal = $(this).index() + 1
+  console.log(indexVal)
+
+  $(".render-table th").removeClass('active')
+  $('.render-table tr td ').removeClass('active')
+
+  $(this).addClass('active')
+  $('.render-table tr td:nth-of-type('+indexVal+')').addClass('active')
+
+  n = $(this).index()
+
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("pepetable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+});
+
+$("#table1searcher").on("keyup", function() {
+  var value = $(this).val().toLowerCase();
+  $("table[table-search-id='"+$(this).attr('id')+"'] tr").filter(function() {
+    if ($(this).find('th').length <= 0) {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    }
+  })
+})
